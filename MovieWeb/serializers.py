30987f7bba13b5bac802b01b174e23acd3023901
Devin Biddy <xp.gram@gmail.com>
2021-08-08 10:26:55 -0700

    add idea

--- MovieWeb/serializers.py
+++ MovieWeb/serializers.py
@@ -10,3 +10,12 @@ class VoteSerializer (serializers.ModelSerializer):
   class Meta:
     model = Vote
     fields = '__all__'
+
+  # def update(self, instance, validated_data):
+  #   instance.movieId = validated_data.get('movieId', instance.movieId)
+  #   vote = validated_data.get('vote', 'Validator Type(?)')
+  #   if vote > 0: instance.upvotes += 1
+  #   if vote < 0: instance.downvotes += 1
+  #   # How do I know if I'm removing a vote?
+  #   instance.save()
+  #   return instance
